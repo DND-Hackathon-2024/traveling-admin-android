156ef13c9908c682d8113edcf85155c5f45cbd75
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-18 09:48:30 +0900

    feat :add new locationitem

--- app/src/main/java/com/plass/travlingadmin/ui/feature/locate/LocateItem.kt
+++ app/src/main/java/com/plass/travlingadmin/ui/feature/locate/LocateItem.kt
@@ -89,3 +89,70 @@ fun LocateItem(
         }
     }
 }
+
+
+@Composable
+fun LocateItem(
+    locate: String,
+    image: String,
+    distance: Int,
+    like: Int,
+    coupon: CouponResponse,
+    onClickItem: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .rippleClickable(onClick = onClickItem)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(start = 12.dp)
+        ) {
+            Spacer(modifier = Modifier.height(8.dp))
+            Row {
+                AsyncImage(
+                    model = image,
+                    modifier = Modifier
+                        .size(64.dp)
+                        .clip(RoundedCornerShape(16.dp)),
+                    contentDescription = "",
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = locate,
+                        style = TravelingTheme.typography.bodyBold,
+                        color = TravelingTheme.colorScheme.Black
+                    )
+                    Spacer(modifier = Modifier.width(2.dp))
+                    Text(
+                        text = distance.toString() + "km 이내",
+                        style = TravelingTheme.typography.labelRegular,
+                        color = TravelingTheme.colorScheme.Gray6
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                }
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier.size(18.dp),
+                    painter = painterResource(id = R.drawable.ic_locate),
+                    contentDescription = "",
+                    colorFilter = ColorFilter.tint(TravelingTheme.colorScheme.Gray98)
+                )
+                Text(
+                    text = (if (like > 99) "99+" else like).toString(),
+                    style = TravelingTheme.typography.labelRegular,
+                    color = TravelingTheme.colorScheme.Gray98
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Coupon(title = "${coupon.couponDiscount} 할인 쿠폰", description = coupon.description, category = coupon.location)
+        }
+    }
+}
